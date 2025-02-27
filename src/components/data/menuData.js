c12a1da8e2ae8dc04f48738996ba935813591c16
FavioG <faviogarciaguzmam@gmail.com>
2025-02-27 15:50:06 -0400

    mejoras de textos y margin

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -6,7 +6,7 @@ export const mainMenu = [
 	// {link:"#blog", content:"Blog", icon:"mdi:blog"}
 ];
 export const menuBlog = [
-	{ link: '/', content: 'Home', icon: 'lucide:home' },
-	{ link: '/#about', content: 'About', icon: 'octicon:person-24' },
-	{ link: '/#portafolio', content: 'Portfolio', icon: 'carbon:portfolio' },
+	{ link: '/', content: 'Inicio', icon: 'lucide:home' },
+	{ link: '/#sobre', content: 'Sobre mí', icon: 'octicon:person-24' },
+	{ link: '/#proyectos', content: 'Proyectos', icon: 'carbon:portfolio' },
 ];
