acba4a4f3fa653a5c6aa1b2ec86f5870227b3e70
FavioG <faviogarciaguzmam@gmail.com>
2025-02-27 14:26:47 -0400

    se mejoraron algunos detalles

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -1,11 +1,12 @@
 export const mainMenu = [
-     {link:"#", content:"Home", icon:"lucide:home"},
-     {link:"#about", content:"About", icon:"octicon:person-24"},
-     {link:"#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
-     {link:"#blog", content:"Blog", icon:"mdi:blog"}
-]
+	{ link: '#', content: 'Inicio', icon: 'lucide:home' },
+	{ link: '#sobre', content: 'Sobre mí', icon: 'octicon:person-24' },
+	// { link: '#skills', content: 'Habilidades', icon: 'octicon:person-24' },
+	{ link: '#proyectos', content: 'Proyectos', icon: 'carbon:portfolio' },
+	// {link:"#blog", content:"Blog", icon:"mdi:blog"}
+];
 export const menuBlog = [
-     {link:"/", content:"Home", icon:"lucide:home"},
-     {link:"/#about", content:"About", icon:"octicon:person-24"},
-     {link:"/#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
-]
+	{ link: '/', content: 'Home', icon: 'lucide:home' },
+	{ link: '/#about', content: 'About', icon: 'octicon:person-24' },
+	{ link: '/#portafolio', content: 'Portfolio', icon: 'carbon:portfolio' },
+];
